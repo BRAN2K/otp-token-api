56c81713df1dd02a9ace35f7f7ba7402871660c3
Guilherme Brante Cavequia <guilherme.cavequia@gmail.com>
2025-07-12 16:20:36 -0300

    fix: conflict Prisma schema path

--- src/infrastructure/database/prisma/PrismaClient.ts
+++ src/infrastructure/database/prisma/PrismaClient.ts
@@ -1,5 +1,5 @@
 import { isProduction } from "@/config/env";
-import { PrismaClient as BasePrismaClient } from "./generated/client";
+import { PrismaClient as BasePrismaClient } from "./generated/prisma/client";
 
 export class PrismaClient extends BasePrismaClient {
   constructor() {
